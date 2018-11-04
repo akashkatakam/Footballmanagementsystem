62d67cc73ad9eac7a5372d5226d3954d2338317a
akashkatakam <katakam.a@husky.neu.edu>
2018-11-03 22:59:04 -0400

    Initial commit Assignment 4 & Lab 8

---
+++ Assignments/Assignment 4/src/assignment_4/entities/Order.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment_4.entities;
+
+/**
+ *
+ * @author harshalneelkamal
+ */
+public class Order {
+    int orderId;
+    int salesId;
+    int customerId;
+    Item item;
+
+    public Order(int orderId, int salesId, int customerId, Item item) {
+        this.orderId = orderId;
+        this.salesId = salesId;
+        this.customerId = customerId;
+        this.item = item;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getSalesId() {
+        return salesId;
+    }
+
+    public void setSalesId(int salesId) {
+        this.salesId = salesId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+    
+    
+}
